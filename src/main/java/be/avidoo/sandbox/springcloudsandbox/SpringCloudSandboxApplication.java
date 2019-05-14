63e2b10689524780e176547598d1730134ec65b5
marcobrasci1986 <marco_brasci@hotmail.com>
2019-05-14 16:03:22 +0200

    initial commit

---
+++ src/main/java/be/avidoo/sandbox/springcloudsandbox/SpringCloudSandboxApplication.java
@@ -0,0 +1,13 @@
+package be.avidoo.sandbox.springcloudsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudSandboxApplication.class, args);
+	}
+
+}
